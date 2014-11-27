f44e7dc6024f17c4315a8cec9130b77604ed65f3
jhsbeat <jhsbeat@gmail.com>
2014-11-27 17:13:40 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125105829) do
+ActiveRecord::Schema.define(version: 20141126062832) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20141125105829) do
   create_table "line_items", force: true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
+    t.integer  "order_id"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
